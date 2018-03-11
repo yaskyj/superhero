b4d46c1351042f962ff9aa88ed5e720506ed73ff
yaskyj <yaskyj@yahoo.com>
2018-03-11 12:48:23 -0500

    Update move file to use random number

--- MoveValidationFiles.py
+++ MoveValidationFiles.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from random import *
 
 heroes = ['ant_man', 'super_man', 'iron_man', 'bat_man', 'captain_america', 
 'spider_man', 'hulk', 'avengers', 'black_panther', 'cat_woman', 'aqua_man', 'ghostrider']
@@ -33,7 +34,7 @@ for i in heroes:
     directory = './CAX_Superhero_Train_Original/' + i
     src_files = os.listdir(directory)
     for x in range(1, int(round(len(os.listdir(directory))))):
-        if x < int(round(len(os.listdir(directory))*.6)):
+        if randint(1, 10) <= 7:
             print x
             dest = './CAX_Superhero_Train_Original/train/' + i + '/' + src_files[x]
             print dest
